e55a76365612ac528eaae9d863808ca40df04800
Januario <jnrdacosta@yahoo.com>
2021-09-13 17:40:24 +0900

    fix error on url.py

--- mpswebsite/settings.py
+++ mpswebsite/settings.py
@@ -153,8 +153,10 @@ USE_TZ = True
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
