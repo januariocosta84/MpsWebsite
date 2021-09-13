ace2eb09ba1acf1f494ed8ba5658041f1f9938b2
Januario <jnrdacosta@yahoo.com>
2021-09-13 17:22:51 +0900

    fix error on url.py

--- mpswebsite/urls.py
+++ mpswebsite/urls.py
@@ -21,6 +21,7 @@ from django.urls import include
 from django.conf.urls import include, re_path
 from django.conf.urls.i18n import i18n_patterns
 from django.utils.translation import ugettext_lazy as _
+from django.views.static import serve
 
 
 urlpatterns = [
